17645c854e1ac004218336d42ea1fd47b6fda451
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-10-17 14:10:19 -0400

    job scheduler

--- oct-15.js
+++ oct-15.js
@@ -3,3 +3,11 @@
 // Implement a job scheduler which takes in a function f and an integer n, 
 //and calls f after n milliseconds.
 
+function jobScheduler(f, n) {
+    setTimeout(f, n)
+}
+const f = () => {
+    console.log("run function! run!")
+}
+const n = 5000
+jobScheduler(f, n)
