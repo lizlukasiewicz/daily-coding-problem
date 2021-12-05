14a647d27e6fe19fd4584cf244250a275c239360
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-12-05 13:10:52 -0500

    december5

---
+++ December/dec-5.py
@@ -0,0 +1,18 @@
+"""
+This problem was asked by Google.
+
+Implement integer exponentiation. 
+That is, implement the pow(x, y) function, 
+where x and y are integers and returns x^y.
+
+Do this faster than the naive method of repeated multiplication.
+
+For example, pow(2, 10) should return 1024.
+"""
+
+
+def exponent(x, y):
+    ans=pow(x,y)
+    print(ans)
+
+exponent(2, 10)
