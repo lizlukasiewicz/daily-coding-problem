c40fbdd125f1ab7daa26cefb5071fecb9b66d206
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-12-07 19:46:58 -0500

    dec 6 and an attempt at Java

--- December/dec-6.py
+++ December/dec-6.py
@@ -8,20 +8,23 @@ You can only move right or down.
 
 For example, given a 2 by 2 matrix, 
 you should return 2, since there are two ways to get to the bottom-right:
-
 Right, then down
 Down, then right
+
 Given a 5 by 5 matrix, 
-there are 70 ways to get to the bottom-right.
+there are 70 ways to get to the bottom-right. (8 moves needed to get from top right to bottom left)
 x x x x x
 x x x x x
 x x x x x
 x x x x x
 x x x x x
 """
-
+import math
+# math.comb (n, k) # of possible combinations of k things from n items
 # math.comb( of 8 things, from 25 items)
+print('2x2 combo is ', math.comb(2, 1))
+print('5x5 combo is', math.comb(8, 4))
 # 
-# k = n * m -2 (2 because the first and last move)
+# moves = n * m -2 (2 because the first and last move)
 # 
 # math.comb(2 things from 4 items) 
---
+++ December/sample.java
@@ -0,0 +1,7 @@
+public class LearnJava {
+    public static void name(String[] args) {
+        String name = "yubaba";
+        System.out.println(name.toUpperCase());
+        
+    }
+}
