5a888d461b620ac9395df71f83334fd7ff9259b9
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2021-12-26 14:26:01 -0600

    dec

--- December/dec-24.py
+++ December/dec-24.py
@@ -11,3 +11,29 @@ return a deepest node. For example, in the following tree, return d.
 d
 
 """
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return f'{self.data}'
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def depth(self, node = None):
+        if not node:
+            node = self.root
+        max_height = 0 
+        def recursive_height(node, height=1):
+            nonlocal max_height
+            if node: 
+                if height> max_height:
+                    max_height = height
+                recursive_height(node.left, height +1)
+                recursive_height(node.right, height+1)
+            recursive_height(node)
+            return max_height
