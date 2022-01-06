1a1e48f6d1d624bbb4a1dff25a6dbf67b3682ebd
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-01-06 00:31:37 -0500

    jann 4-5 still need solve

---
+++ January/jan-4.py
@@ -0,0 +1,24 @@
+"""
+This problem was asked by Dropbox.
+
+What does the below code snippet print out? 
+How can we fix the anonymous functions to behave as we'd expect? 
+
+functions = []
+for i in range(10):
+    functions.append(lambda : i)
+
+for f in functions:
+    print(f())
+
+"""
+functions = []
+
+for i in range(10):
+    functions.append(i)
+    print(i, "🦋")
+
+for f in functions:
+    print(f)
+
+
