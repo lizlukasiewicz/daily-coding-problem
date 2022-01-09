36c48aac81ab33ed12872aaefeaed5ed5d018aaa
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-01-09 18:45:54 -0500

    dec

--- December/dec-15.py
+++ December/dec-15.py
@@ -5,3 +5,11 @@ Using a function rand7() that returns an integer from 1 to 7 (inclusive)
 with uniform probability, 
 implement a function rand5() that returns an integer from 1 to 5 (inclusive).
 """
+import random
+
+def rand7():
+    print(random.randint(1, 7), "🎈")
+rand7()
+def rand5():
+    print(random.randint(1, 5), "👹")
+rand5()
