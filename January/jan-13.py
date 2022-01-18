0880125a7cec3d438b1e9905c13f3436ef7af85b
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-01-18 17:09:57 -0500

    debounced

--- January/jan-13.py
+++ January/jan-13.py
@@ -22,4 +22,7 @@ Output: 2
 It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move from (1, 1) to (1, 2).
 """
 
+
+
 #calculate 
+test = [(0, 0), (1, 1), (1, 2)]
---
+++ January/jan-18.py
@@ -0,0 +1,29 @@
+"""
+This problem was asked by Facebook.
+
+Given a function f, and N return a debounced f of N milliseconds.
+
+That is, as long as the debounced f continues to be invoked, 
+f itself will not be called for N milliseconds.
+"""
+#Debouncing means to supress the call to a function within a given timeframe, 
+# say you call a function 100 times within 1 second 
+# but you only want to allow the function to run once every 10 seconds 
+# a debounce decorated function would run the function once 10 seconds after 
+# the last function call if no new function calls
+
+import threading
+
+def debounce(N):
+    def decorator(f):
+        def debounced(*args, **kwargs):
+            def call_f():
+                debounced._timer = None
+                return function(*args, **kwargs)
+            if debounced._timer is not None:
+                debounced._timer.cancel()
+            debounced._timer = threading.Timer(N, call_f)
+            debounced._timer.start()
+        debounced._timer = None
+        return debounced
+    return decorator
