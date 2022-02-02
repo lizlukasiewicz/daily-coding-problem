1338a7091ac82a22f34a4239ef801586170145c8
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-02-02 14:59:07 -0500

    feb 2 incomplete

---
+++ February/feb-1.py
@@ -0,0 +1,13 @@
+"""
+This problem was asked by Google.
+
+Given a sorted list of integers, square the elements and give the output in sorted order.
+
+For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
+"""
+def sorted_square(list):
+    ans =sorted([y**2 for y in list])
+    print(ans)
+
+test = [-9, -2, 0, 2, 3]
+sorted_square(test)
