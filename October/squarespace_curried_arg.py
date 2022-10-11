d1cd069a20c2b593e9fb7be77b5178402271fc2c
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-10-11 11:46:57 -0400

    october addition

--- October/squarespace_curried_arg.py
+++ October/squarespace_curried_arg.py
@@ -14,28 +14,6 @@ add_subtract(-5)(10)(3)(9) -> -5 + 10 - 3 + 9 -> 11
 """
 
 from __future__ import annotations
-# from unittest import result
-
-# class CallableInt(int):
-#     def __init__(self, value: int) -> None:
-#         int.__init__(value)
-#         self.should_add = True
-
-#     def __call__(self, value: int) -> CallableInt:
-#         if self.should_add:
-#             result = CallableInt(self + value)
-#         else:
-#             result = CallableInt(self - value)
-#         result.update_should_add(not self.should_add)
-#         return result
-
-#     def update_should_add(self, should_add: bool) -> None:
-#         self.should_add = should_add
-
-
-# def add_subtract(value: int) -> CallableInt:
-#     return CallableInt(value)
-
 
 
 class CurriedInt(int):
