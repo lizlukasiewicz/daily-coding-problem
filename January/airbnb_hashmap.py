9db128c9e4aa5d7c74037a236572a3d350bb45fe
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-10-23 21:33:52 -0400

    oct

--- January/airbnb_hashmap.py
+++ January/airbnb_hashmap.py
@@ -8,8 +8,32 @@ Return a sorted ordering of courses such that we can finish all courses.
 Return null if there is no such ordering.
 
 For example, given
- {'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100'], 'CSC100': []}, 
+ {
+    'CSC300': ['CSC100', 'CSC200'], 
+    'CSC200': ['CSC100'],  
+    'CSC100': []
+    }, 
  should return ['CSC100', 'CSC200', 'CSCS300'].
 
 
 """
+
+def course_order(hashmap):
+    order=[]
+    for course in hashmap:
+        for prereq in hashmap[course]:
+            if hashmap[prereq]:
+                order.insert(0, prereq)
+            else:
+                order.append(prereq)
+        order.append(course)
+    print(order)
+
+courseId={
+    'CSC300': ['CSC100', 'CSC200'], 
+    'CSC200': ['CSC100'],  
+    'CSC100': []
+    }
+course_order(courseId)
+        # if len(hashmap[course])==0:
+        #     order.insert(0, course)
