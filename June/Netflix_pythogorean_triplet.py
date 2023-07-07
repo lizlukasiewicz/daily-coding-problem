9cc4aadf84ce75404c9fd178ab849641e826a74d
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2023-07-06 20:31:58 -0400

    june

--- June/Netflix_pythogorean_triplet.py
+++ June/Netflix_pythogorean_triplet.py
@@ -8,6 +8,22 @@ Recall that a Pythogorean triplet (a, b, c) is defined by the equation a2+ b2= c
 """
 
 def pythogo(l:list):
-    for x in l:
-        print(x)
-    
+    n=len(l)
+    for i in range(n):
+      l[i]= l[i]*l[i]
+    l.sort()
+
+    for i in range(n-1, 1, -1):
+      s=set()
+      for j in range(i - 1, -1, -1):
+        if (l[i] - l[j]) in s:
+          return True
+        s.add(l[j])
+    return False
+      
+arr = [10, 4, 6, 12, 5]
+
+if (pythogo(arr)):
+  print("True")
+else:
+  print("False")
