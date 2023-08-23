a3f80db25e6c69be7dbb56ba94161f14540355ff
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2023-08-23 19:43:48 -0400

    catchup;practice

--- January/google_string_anag.py
+++ January/google_string_anag.py
@@ -7,3 +7,20 @@ For example, given that W is "ab", and S is "abxaba", return 0, 3, and 4.
 
 
 """
+
+def anagram(W: str, S:str):
+    window=len(W)
+    left=0
+    empty_array=[]
+    char=set(W)
+    # optimize? :: make it recursive
+    while window <= len(S):
+        y=char.symmetric_difference(set(S[left:window]))
+        if y==set():
+          empty_array.append(left)
+        left+=1
+        window+=1
+    print(empty_array)
+    return empty_array
+
+anagram(W="ab", S="abxaba")
